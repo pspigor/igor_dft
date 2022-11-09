d20e8a37bc7df8aeeee23b7c35e5d7e7e22ec3e6
pspigor <igorpsp96@gmail.com>
2022-11-09 15:21:32 -0300

    delete egg-info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="igor_dft",
-    version="0.0.3",
+    version="0.0.4",
     author="Igor Pereira", #<<<
     author_email="ipereira@peq.coppe.ufrj.br", #<<<
     description="A small cdft package", #<<<
---
+++ teste-dft/testeinstall.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  9 14:36:26 2022
+
+@author: igorp
+"""
+    
+import igor_dft
+from igor_dft import DFTplotsresultsATT
